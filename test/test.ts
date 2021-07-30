4c773ead6289b5c7ab47d3f293c9176a923cea4d
akoll <31693992+akoll@users.noreply.github.com>
2021-07-30 02:19:50 +0200

    Add negative type-safety tests and cardinality tests

--- test/test.ts
+++ test/test.ts
@@ -3,13 +3,63 @@ import { expect } from 'chai';
 import { BidirectionalMap } from '../src/bidirectional_map';
 import { biject } from '../src/bijection';
 
+type Equals<T, V> = [T] extends [V] ? [V] extends [T] ? true : false : false;
+
 describe('biject', () => {
   it('returns the BidirectionalMap constructor', () => {
-    const constructor1 = biject<'a' | 'b' | 'c', 1 | 2 | 3>();
-    expect(constructor1).to.equal(BidirectionalMap);
+    expect(
+      biject<'a' | 'b' | 'c', 1 | 2 | 3>()
+    ).to.equal(BidirectionalMap);
 
-    const constructor2 = biject<'kek' | 'lel' | 5 | 3.14, `${string}${number}` | 5 | 'pip' | false>();
-    expect(constructor2).to.equal(BidirectionalMap);
+    expect(
+      biject<'kek' | 'lel' | 5 | 3.14, `${string}${number}` | 5 | 'pip' | false>()
+    ).to.equal(BidirectionalMap);
+  });
+
+  it('asserts equal set cardinality', () => {
+    new (
+      // @ts-expect-error Set cardinalities don't match.
+      biject<'a' | 'b', 1 | 2 | 3>()
+    )([]);
+  });
+
+  it('asserts injectiveness', () => {
+    new (biject<'a' | 'b' | 'c', 1 | 2 | 3>())(
+      // @ts-expect-error Given map is not injective.
+      <const>[
+        ['a', 1],
+        ['b', 2],
+        ['c', 1],
+      ]
+    );
+
+    new (biject<'a' | 'b' | 'c', 1 | 2 | 3>())(
+      <const>[
+        ['a', 3],
+        ['b', 2],
+        ['c', 1],
+      ]
+    );
+  });
+
+  it('asserts surjectiveness', () => {
+    new (biject<'a' | 'b', 1 | 2 | 3>())(
+      // @ts-expect-error Given map is not surjective.
+      <const>[
+        ['a', 1],
+        ['b', 2],
+      ]
+    );
+
+    // @ts-expect-error Set cardinalities don't match.
+    new (biject<'a' | 'b', 1 | 2 | 3>())(
+      // But the map is surjective.
+      <const>[
+        ['a', 1],
+        ['b', 2],
+        ['b', 3],
+      ]
+    );
   });
 
   it('infers the image type on <const> maps', () => {
@@ -47,7 +97,6 @@ describe('biject', () => {
     const element = bijection.map('c');
     expect(element).to.equal(2);
 
-    type Equals<T, V> = [T] extends [V] ? [V] extends [T] ? true : false : false;
     const elementTypeUnknown: Equals<typeof element, 1 | 2 | 3> = true;
     expect(elementTypeUnknown).to.equal(true);
   });
@@ -63,8 +112,18 @@ describe('biject', () => {
     const element = bijection.invert(1);
     expect(element).to.equal('b');
 
-    type Equals<T, V> = [T] extends [V] ? [V] extends [T] ? true : false : false;
     const elementTypeUnknown: Equals<typeof element, 'a' | 'b' | 'c'> = true;
     expect(elementTypeUnknown).to.equal(true);
+  });
+
+  it('works with big sets', () => {
+    new (biject<
+      1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100,
+      1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100
+    >())(
+      <const>[
+        [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], [11, 11], [12, 12], [13, 13], [14, 14], [15, 15], [16, 16], [17, 17], [18, 18], [19, 19], [20, 20], [21, 21], [22, 22], [23, 23], [24, 24], [25, 25], [26, 26], [27, 27], [28, 28], [29, 29], [30, 30], [31, 31], [32, 32], [33, 33], [34, 34], [35, 35], [36, 36], [37, 37], [38, 38], [39, 39], [40, 40], [41, 41], [42, 42], [43, 43], [44, 44], [45, 45], [46, 46], [47, 47], [48, 48], [49, 49], [50, 50], [51, 51], [52, 52], [53, 53], [54, 54], [55, 55], [56, 56], [57, 57], [58, 58], [59, 59], [60, 60], [61, 61], [62, 62], [63, 63], [64, 64], [65, 65], [66, 66], [67, 67], [68, 68], [69, 69], [70, 70], [71, 71], [72, 72], [73, 73], [74, 74], [75, 75], [76, 76], [77, 77], [78, 78], [79, 79], [80, 80], [81, 81], [82, 82], [83, 83], [84, 84], [85, 85], [86, 86], [87, 87], [88, 88], [89, 89], [90, 90], [91, 91], [92, 92], [93, 93], [94, 94], [95, 95], [96, 96], [97, 97], [98, 98], [99, 99], [100, 100],
+      ],
+    );
   });
 });
