ada68f898543aa09d164c5b3e637437ac548ed30
akoll <31693992+akoll@users.noreply.github.com>
2022-01-06 21:42:03 +0100

    Change cardinality tests to expect errors in map definitions

--- test/test.ts
+++ test/test.ts
@@ -18,9 +18,22 @@ describe('biject', () => {
 
   it('asserts equal set cardinality', () => {
     new (
-      // @ts-expect-error Set cardinalities don't match.
       biject<'a' | 'b', 1 | 2 | 3>()
-    )([]);
+    // @ts-expect-error Domain contains duplicates.
+    )(<const>[
+      ['a', 1],
+      ['b', 2],
+      ['a', 3],
+    ]);
+
+    new (
+      biject<'a' | 'b' | 'c', 1 | 2>()
+    // @ts-expect-error Image contains duplicates.
+    )(<const>[
+      ['a', 1],
+      ['b', 2],
+      ['c', 1],
+    ]);
   });
 
   it('asserts injectiveness', () => {
@@ -48,16 +61,6 @@ describe('biject', () => {
       <const>[
         ['a', 1],
         ['b', 2],
-      ]
-    );
-
-    // @ts-expect-error Set cardinalities don't match.
-    new (biject<'a' | 'b', 1 | 2 | 3>())(
-      // But the map is surjective.
-      <const>[
-        ['a', 1],
-        ['b', 2],
-        ['b', 3],
       ]
     );
   });
