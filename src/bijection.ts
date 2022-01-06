1b35b17f75f03537351f6583b3c424e0b1f86ea9
akoll <31693992+akoll@users.noreply.github.com>
2022-01-06 23:42:10 +0100

    Switch to string-based error message for non-surjective maps

--- src/bijection.ts
+++ src/bijection.ts
@@ -2,9 +2,6 @@ import { Pairs, ImageElement, DomainElement } from './utility/pairs';
 import { First, Rest, Unique } from './utility/tuple';
 import { BidirectionalMap } from './bidirectional_map';
 
-type SurjectiveMap<Present, Missing> = readonly (readonly [unknown, Missing])[];
-type InjectiveMap<Present, Missing> = readonly (readonly [Missing, unknown])[];
-
 type Remove<Ts, T> = First<Ts> extends T ? Rest<Ts> : [First<Ts>, ...Remove<Rest<Ts>, T>];
 type Subtract<Minuend, Subtrahend> = Subtrahend extends readonly [] ? Minuend : Subtract<Remove<Minuend, First<Subtrahend>>, Rest<Subtrahend>>;
 
@@ -12,7 +9,6 @@ type Subtract<Minuend, Subtrahend> = Subtrahend extends readonly [] ? Minuend : 
 // type AssertInjectiveness<T extends Pairs, Domain> = [Domain] extends [DomainElement<T>] ? T : InjectiveMap<Extract<DomainElement<T>, Domain>, Exclude<Domain, DomainElement<T>>>;
 
 type RemoveUniqueImageElements<T extends Pairs, Current = T, Accumulator extends readonly unknown[] = readonly []> = Current extends readonly [] ? Accumulator : (Current extends readonly [infer First, ...infer Rest] ? (First extends readonly [unknown, infer Right] ? (Unique<T, readonly [unknown , Right]> extends false ? RemoveUniqueImageElements<T, Rest, [...Accumulator, First]> : RemoveUniqueImageElements<T, Rest, Accumulator>) : never) : never);
-
 type RemoveUniqueDomainElements<T extends Pairs, Current = T, Accumulator extends readonly unknown[] = readonly []> = Current extends readonly [] ? Accumulator : (Current extends readonly [infer First, ...infer Rest] ? (First extends readonly [infer Left, unknown] ? (Unique<T, readonly [Left, unknown]> extends false ? RemoveUniqueDomainElements<T, Rest, [...Accumulator, First]> : RemoveUniqueDomainElements<T, Rest, Accumulator>) : never) : never);
 
 type AssertFunction<T extends Pairs> = RemoveUniqueDomainElements<T> extends readonly [] ? T : {
@@ -23,7 +19,9 @@ type AssertInjectiveness<T extends Pairs> = RemoveUniqueImageElements<T> extends
   'Some image elements (right side) are ambiguous (mapped more than once)': ImageElement<RemoveUniqueImageElements<T>>;
 };
 
-type AssertSurjectiveness<T extends Pairs, Codomain> = [Codomain] extends [ImageElement<T>] ? T : SurjectiveMap<ImageElement<T>, Exclude<Codomain, ImageElement<T>>>;
+type AssertSurjectiveness<T extends Pairs, Codomain> = [Codomain] extends [ImageElement<T>] ? T : {
+  'Some image elements are missing (function is not surjective)': Exclude<Codomain, ImageElement<T>>;
+};
 
 export function biject<Domain, Codomain>() {
   return BidirectionalMap as new <T extends readonly (readonly [Domain, Codomain])[]>(
